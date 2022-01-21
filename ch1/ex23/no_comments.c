af5f88d97d5d6d5dfc9e938b48743f6fd401925a
Michael Singhurse <michaels@acst.com>
2022-01-21 15:22:43 -0800

    almost complete

--- ch1/ex23/no_comments.c
+++ ch1/ex23/no_comments.c
@@ -2,6 +2,13 @@
 
 #define MAXLINE 1000
 
+/*
+ * TODO:
+ * x double slash single line comment
+ * - slash-asterisk comment on single-line
+ * - slash-asterisk comment on multi-lines
+ */
+
 int mygetline(char s[], int m);
 
 int main() {
@@ -9,43 +16,67 @@ int main() {
   char current[MAXLINE];
   int in_double_quotes = 0;
   int in_single_quotes = 0;
-  int was_slash = 0;
   int in_block_comment = 0;
-  int was_asterisk = 0;
+  int prev_c = -1;
+  int break_loop;
 
   while (mygetline(current, MAXLINE) > 0) {
     for (i = 0; (c = current[i]) && c != '\0'; ++i) {
-      if (c == '\"' && in_double_quotes)
-        in_double_quotes = 0;
-      else if (c == '\"' && !in_double_quotes)
-        in_double_quotes = 1;
+      switch (c) {
+        case '\"':
+          if (in_double_quotes)
+            in_double_quotes = 0;
+          else if (!in_single_quotes)
+            in_double_quotes = 1;
+          break;
 
-      if (c == '\'' && in_single_quotes)
-        in_single_quotes = 0;
-      else if (c == '\'' && !in_single_quotes)
-        in_single_quotes = 1;
-    
-      if (c == '*' && was_slash && !in_double_quotes && !in_single_quotes) {
-        in_block_comment = 1;
-        was_slash = 0;
-        current[i - 1] = '\n';
-        current[i] = '\0';
-      } else if (c == '*' && in_block_comment && !in_double_quotes && !in_single_quotes)
-        was_asterisk = 1;
-      else if (c == '/' && in_block_comment && was_asterisk && !in_double_quotes && !in_single_quotes) {
+        case '\'':
+          if (in_single_quotes)
+            in_single_quotes = 0;
+          else if (!in_double_quotes)
+            in_single_quotes = 1;
+          break;
 
-      if (c != '*')
-        was_asterisk = 0;
+        case '/':
+          if (prev_c == '/' && !in_double_quotes && !in_single_quotes && !in_block_comment) {
+            current[i - 1] = '\n';
+            current[i] = '\0';
+            break_loop = 1;
+          }
+          else if (prev_c == '*' && in_block_comment) {
+            current[0] = '\n';
+            current[1] = '\0';
+            in_block_comment = 0;
+            break_loop = 1;
+          }
+          break;
 
- 
-      if (c == '/' && !was_slash)
-        was_slash = 1;
-      else if (c == '/' && was_slash && !in_double_quotes && !in_single_quotes) {
-        current[i - 1] = '\n';
-        current[i] = '\0';
-      } else
-        was_slash = 0;
+        case '*':
+          if (prev_c == '/' && !in_double_quotes && !in_single_quotes) {
+            in_block_comment = 1;
+            current[i - 1] = '\n';
+            current[i] = '\0';
+            break_loop = 1;
+          }
+          break;
+      }
+
+      if (break_loop) {
+        break_loop = 0;
+        break;
+      }
+
+      prev_c = c;
     }
+
+    if (in_block_comment) {
+      current[0] = '\n';
+      current[1] = '\0';
+    }
+
+    in_double_quotes = 0;
+    in_single_quotes = 0;
+    prev_c = -1;
 
     printf("%s", current);
   }
