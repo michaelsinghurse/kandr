1159c70efc9189253a4ccb87a9f1a0a6a79df1e1
Michael Singhurse <michaels@acst.com>
2022-01-28 14:21:55 -0800

    completed exercise 2.3

--- ch2/ex3/main.c
+++ ch2/ex3/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-char[] assertequal(int a, int b);
+int assertequal(int a, int b);
 int htoi(const char s[]);
 
 int main() {
+  int pass;
   char a[] = "0x001";
   int aint = 1;
   char b[] = "0X001";
   int bint = 1;
-  char c[] = "001";
-  int cint = 1;
+  char c[] = "0X000";
+  int cint = 0;
   char d[] = "1";
   int dint = 1;
   char e[] = "0";
@@ -18,8 +19,8 @@ int main() {
   int fint = 17;
   char g[] = "0X011";
   int gint = 17;
-  char h[] = "011";
-  int hint = 17;
+  char h[] = "111";
+  int hint = 273;
   char i[] = "0x100";
   int iint = 256; 
   char j[] = "0x123";
@@ -27,21 +28,115 @@ int main() {
   char k[] = "0x00000000000000000000001";
   int kint = 1;
 
-  printf("Converts %s to %d? %s\n", a, aint, assertequal(htoi(a), aint)); 
+  printf("Converts %s to %d? ", a, aint);
+  pass = assertequal(htoi(a), aint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
 
+  printf("Converts %s to %d? ", b, bint);
+  pass = assertequal(htoi(b), bint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
 
-    
+  printf("Converts %s to %d? ", c, cint);
+  pass = assertequal(htoi(c), cint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
+
+  printf("Converts %s to %d? ", d, dint);
+  pass = assertequal(htoi(d), dint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
+
+  printf("Converts %s to %d? ", e, eint);
+  pass = assertequal(htoi(e), eint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
+
+  printf("Converts %s to %d? ", f, fint);
+  pass = assertequal(htoi(f), fint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
+
+  printf("Converts %s to %d? ", g, gint);
+  pass = assertequal(htoi(g), gint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
+
+  printf("Converts %s to %d? ", h, hint);
+  pass = assertequal(htoi(h), hint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
+
+  printf("Converts %s to %d? ", i, iint);
+  pass = assertequal(htoi(i), iint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
+
+  printf("Converts %s to %d? ", j, jint);
+  pass = assertequal(htoi(j), jint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
+
+  printf("Converts %s to %d? ", k, kint);
+  pass = assertequal(htoi(k), kint);
+  if (pass)
+    printf("%s\n", "PASS");
+  else
+    printf("%s\n", "FAIL");
 
   return 0;
 }
 
 int htoi(const char hex[]) {
-  return 1;
+  char c;
+  int i;
+  int d;
+  int result = 0;
+
+  if (hex[0] == '0' && (hex[1] == 'x' || hex[1] == 'X'))
+    i = 2;
+  else
+    i = 0;
+
+  while ((c = hex[i]) != '\0') {
+    if (c >= '0' && c <= '9')
+      d = c - '0';
+    else if (c >= 'a' && c <= 'f')
+      d = 10 + c - 'a';
+    else if (c >= 'A' && c <= 'F')
+      d = 10 + c - 'A';
+    else
+      // invalid character
+      return -1; 
+
+    result = 16 * result + d;
+    ++i;
+  }
+
+  return result;
 }
 
-char[] assertequal(int a, int b) {
-  if (a == b)
-    return "PASS";
-  else
-    return "FAIL";
+int assertequal(int a, int b) {
+  return a == b;
 }
---
+++ ch2/ex3/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("a: %d\n", 'a');
+  printf("z: %d\n", 'z');
+  printf("A: %d\n", 'A');
+  printf("Z: %d\n", 'Z');
+  printf("0: %d\n", '0');
+  printf("9: %d\n", '9');
+
+  return 0;
+}
