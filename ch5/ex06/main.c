fb535503bd62c23da90848edabbfda9e0d38f5bf
Michael Singhurse <michaels@acst.com>
2022-03-04 15:29:18 -0800

    completed 5.6

--- ch5/ex06/main.c
+++ ch5/ex06/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define BUFSIZE 100
+#define MAXLINE 1000
+#define NUMBER '0'
+
 int mygetline(char *, int);
 int atoi(char *);
 void reverse(char *);
 void itoa (int, char *);
+int strindex(char *, char *);
+int getop(char *);
 
-/*
- * strindex (ch 4)
- * getop (ch 4)
- */
-
-#define MAXLINE 1000
 
 int main(int argc, char *argv[])
 {
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
   while (mygetline(s, MAXLINE) > 0)
     printf("%s", s);
   */
-  
+
   /*
   // TEST atoi
   printf("atoi non-digit returns 0: %d\n", atoi("four"));
@@ -47,8 +47,93 @@ int main(int argc, char *argv[])
   itoa(n, s);
   printf("itoa on %d produces %s\n", n, s);
   */
+  
+  /*
+  // TEST strindex
+  char *s = "hello";
+  char *t = "lo";
+  printf("the index of '%s' in '%s' is %d\n", t, s, strindex(s, t));
+
+  char *u = "xyz";
+  printf("the index of '%s' in '%s' is %d\n", u, s, strindex(s, u));
+  */
+
+  /*
+  // TEST getop
+  int type;
+  char s[100];
+
+  while ((type = getop(s)) != EOF)
+    if (type == NUMBER)
+      printf("NUMBER: %s\n", s);
+    else if (type != '\n')
+      printf("OP: %s\n", s);
+  */
 
   return 0;
+}
+
+char buf[BUFSIZE];
+char *pb = buf;
+
+int getch(void)
+{
+  return (pb > buf) ? *--pb : getchar();
+}
+
+void ungetch(int c)
+{
+  if (pb >= buf + BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    *pb++ = c;    
+}
+
+int getop(char *s)
+{
+  int c;
+
+  while ((*s = c = getch()) == ' ' || c == '\t')
+    ;
+
+  *++s = '\0';
+
+  if (!isdigit(c) && c != '.')
+    return c;
+
+  if (isdigit(c))
+    for ( ; isdigit(*s = c = getch()); s++)
+      ;
+
+  if (c == '.')
+    for ( ; isdigit(*s = c = getch()); s++)
+      ;
+
+  *s = '\0';
+
+  if (c != EOF)
+    ungetch(c);
+
+  return NUMBER;
+}
+
+int strindex(char *s, char *t)
+{
+  char *i, *ps, *pt;
+
+  ps = s;
+  pt = t;
+
+  for ( ; *s != '\0'; s++) {
+    t = pt;
+    for (i = s; *t != '\0' && *i == *t; i++, t++)
+      ;
+
+    if (t > pt && *t == '\0')
+      return s - ps;
+  }
+
+  return -1;
 }
 
 void itoa(int n, char *s)
