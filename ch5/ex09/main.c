ec38c596eaec517b71011a2c13f21649d4e95d1e
Michael Singhurse <michaelsinghurse@gmail.com>
2022-03-11 06:08:53 -0800

    completed ex 5.9

---
+++ ch5/ex09/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int day_of_year(int, int, int);
+int month_day(int , int , int *, int *);
+
+int main(int argc, char *argv[])
+{
+  printf("Today is day %d of the year.\n", day_of_year(2022, 3, 11));
+  printf("Jan 1 is day %d of the year.\n", day_of_year(2022, 1, 1));
+  printf("Dec 31 is day %d of the year.\n", day_of_year(2022, 12, 31));
+
+  int month; 
+  int day;
+  
+  month_day(-10, 69, &month, &day);
+  month_day(2022, 0, &month, &day);
+  month_day(2022, 366, &month, &day);
+  
+  month_day(2022, 70, &month, &day);
+  printf("Day 70 of 2022 is month %d day %d.\n", month, day);
+  month_day(2022, 365, &month, &day);
+  printf("Day 365 of 2022 is month %d day %d.\n", month, day);
+
+  return 0;
+}
