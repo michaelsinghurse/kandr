3edfb3a9d8996dda79222239f396c26318b6c6cb
Michael Singhurse <michaelsinghurse@gmail.com>
2022-03-23 06:18:00 -0700

    completed ex 5.14

---
+++ ch5/ex14/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines, int reverse);
+
+void qsort(void *lineptr[], int left, int right,
+           int (*comp)(void *, void *));
+int numcmp(const char *, const char *);
+
+int main(int argc, char *argv[])
+{
+  char c;
+  int nlines, numeric = 0, reverse = 0;
+
+  while (--argc > 0 && (*++argv)[0] == '-')
+    while (c = *++argv[0])
+      switch (c) {
+        case 'n':
+          numeric = 1;
+          break;
+        case 'r':
+          reverse = 1;
+          break;
+        default:
+          printf("invalid option: %c\n", c);
+          return 1;
+      }
+
+  if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    qsort((void **) lineptr, 0, nlines-1,
+          (int (*)(void *, void *))(numeric ? numcmp : strcmp));
+    writelines(lineptr, nlines, reverse);
+    return 0;
+  } else {
+    printf("input too big to sort\n");
+    return 1;
+  }
+}
