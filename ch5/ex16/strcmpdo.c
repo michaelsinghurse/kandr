c13f4d55777129fa7aa4354541f77a58ecf619ba
Michael Singhurse <michaels@acst.com>
2022-03-23 10:42:03 -0700

    correction to directory order sort

--- ch5/ex16/strcmpdo.c
+++ ch5/ex16/strcmpdo.c
@@ -7,26 +7,36 @@ static int isdochar(char c)
 
 int strcmpdo(const char *s, const char *t)
 {
-  while (*s != '\0') {
-    if (isdochar(*s) && isdochar(*t) && *s != *t)
+  while (*s != '\0' && *t != '\0')
+    if (!isdochar(*s))
+      s++;
+    else if (!isdochar(*t))
+      t++;
+    else {
+      if (*s != *t)
         return *s - *t;
- 
-    s++;
-    t++;
-  }
 
-  return *t == '\0' ? 0 : (*s - *t);
+      s++;
+      t++;
+    }
+
+  return *s - *t;
 }
 
 int strcmpdoci(const char *s, const char *t)
 {
-  while (*s != '\0') {
-    if (isdochar(*s) && isdochar(*t) && tolower(*s) != tolower(*t))
+  while (*s != '\0' && *t != '\0')
+    if (!isdochar(*s))
+      s++;
+    else if (!isdochar(*t))
+      t++;
+    else {
+      if (tolower(*s) != tolower(*t))
         return tolower(*s) - tolower(*t);
- 
-    s++;
-    t++;
-  }
 
-  return *t == '\0' ? 0 : (tolower(*s) - tolower(*t));
+      s++;
+      t++;
+    }
+
+  return tolower(*s) - tolower(*t);
 }
