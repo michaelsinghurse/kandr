ffb0169b764bf61df7d5c3145f35fc21300034ab
Michael Singhurse <michaels@acst.com>
2022-04-04 09:13:06 -0700

    completed exercise 7.6

--- ch7/ex06/main.c
+++ ch7/ex06/main.c
@@ -1,32 +1,23 @@
-/*
- * - parse CL args to get file names
- * - declare and initialize lineno to 1
- * - open each file
- * - while getline from each file
- * - strcmp on each line.
- *   - if 0, continue
- *   - if not 0, print file name, lineno, and line of each
- *
- */
 #include <stdio.h>
 #include <string.h>
 
 #define MAXLINE 1000
 
 /* compare two files, print first line of each where they differ */
-int main(int argc, char argv**)
+int main(int argc, char **argv)
 {
   int lineno, status;
   FILE *pf1, *pf2;
   char l1[MAXLINE], l2[MAXLINE];
   char *prog, *f1, *f2;
 
+  prog = argv[0];
+
   if (argc != 3) {
     fprintf(stderr, "usage: %s file1 file2\n", prog);
     return 1;
   }
 
-  prog = argv[0];
   f1 = argv[1];
   f2 = argv[2];
 
@@ -55,13 +46,13 @@ int main(int argc, char argv**)
   if (ferror(pf1)) {
     fprintf(stderr, "error reading %s\n", f1);
   } else if (feof(pf1) && !feof(pf2)) {
-    printf("%s [%d]: End Of File\n", f1, lineno);
-    printf("%s [%d]: %s", f2, lineno, l2);
+    printf("All of the lines in %s are the same as the first %d lines in %s\n",
+           f1, lineno, f2);
   } else if (ferror(pf2)) {
     fprintf(stderr, "error reading %s\n", f2);
   } else if (!feof(pf1) && feof(pf2)) {
-    printf("%s [%d]: %s", f1, lineno, l1);
-    printf("%s [%d]: End Of File\n", f2, lineno);
+    printf("All of the lines in %s are the same as the first %d lines in %s\n",
+           f2, lineno, f1);
   }
 
   status = (ferror(pf1) || ferror(pf2)) ? 1 : 0;
@@ -71,24 +62,4 @@ int main(int argc, char argv**)
 
   return status;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
